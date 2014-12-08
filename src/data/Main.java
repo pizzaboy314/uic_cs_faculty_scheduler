15ae4109fd238ebcaf582b30ad87729b467610d9
Bryan Spahr <bspahr314@gmail.com>
2014-12-07 22:26:11 -0600

    import cleanup

--- src/data/Main.java
+++ src/data/Main.java
@@ -1,19 +1,5 @@
 package data;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class Main {
 	
 	public static void main(String[] args) {
